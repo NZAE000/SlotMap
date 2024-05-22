5012c1d08daad0e153082469348716c8de034e1b
NZAE000 <eliezer.zuniga@alumnos.uv.cl>
2024-05-21 22:57:01 -0400

    main function fixed updated.

--- src/main.cpp
+++ src/main.cpp
@@ -18,34 +18,29 @@ int main(void)
     showMemoryObject(m_vec.vec, sizeof(m_vec.vec));
 
 // To show the data stored by std::vector (heap)
-    std::size_t size {48};
-    std::printf("\n************************* Size object: %lu ************************\n\n", size);
-    showMemory(reinterpret_cast<uint8_t const*>(&*m_vec.vec.begin()), size);
+    showMemoryObject(*m_vec.vec.begin(), 48);
 
-    //// To show how the array is stored in memory(stack).
-    //std::array arr { 1, 2, 4, 8, 16, 32, 64};
-    //showMemoryObject(arr);
+//    // To show how the array is stored in memory (stack).
+//    std::array arr { 1, 2, 4, 8, 16, 32, 64};
+//    showMemoryObject(arr, sizeof(arr));
 //
-    //// To show how the vector is stored in memory(stack).
-    //std::vector vec { 1, 2, 4, 8, 16, 32}; // remember: vector { ptr_first_elem, ptr_post_end_elem, ptr_end_capacity}
-    //showMemoryObject(vec);
+//    // To show how the vector is stored in memory (stack).
+//    std::vector vec { 1, 2, 4, 8, 16, 32}; // remember: vector { ptr_first_elem, ptr_post_end_elem, ptr_end_capacity}
+//    showMemoryObject(vec, sizeof(vec));
 //
-    //std::printf("begin vec = %16p\n", (void*)(&*vec.begin())); // * to access to content, then with & to access to his address.
-    //std::printf("end vec   = %16p\n", (void*)(&*vec.end()));
+//    std::printf("begin vec = %16p\n", (void*)(&*vec.begin())); // * to access to content, then with & to access to his address.
+//    std::printf("end vec   = %16p\n", (void*)(&*vec.end()));
 //
-    //// To show the elements of vector
-    //std::size_t size {48};
-    //std::printf("\n************************* Size object: %lu ************************\n\n", size);
-    //showMemory(reinterpret_cast<uint8_t const*>(&*vec.begin()), size);
+//    // To show the elements of vector (heap)
+//    showMemoryObject(*vec.begin(), 48);
 //
-    //vec.push_back(64);
-    //showMemoryObject(vec);
+//    vec.push_back(64);
+//    showMemoryObject(vec, sizeof(vec));
 //
-    //std::printf("begin vec = %16p\n", (void*)(&*vec.begin())); // * to access to content, then with & to access to his address.
-    //std::printf("end vec   = %16p\n", (void*)(&*vec.end()));
+//    std::printf("begin vec = %16p\n", (void*)(&*vec.begin())); // * to access to content, then with & to access to his address.
+//    std::printf("end vec   = %16p\n", (void*)(&*vec.end()));
 //
-    //std::printf("\n************************* Size object: %lu ************************\n\n", size);
-    //showMemory(reinterpret_cast<uint8_t const*>(&*vec.begin()), size);
-//
+//    showMemoryObject(*vec.begin(), 48);
+
     return 0;
 }
